14b048550b7f87ec62e5193899a46e92c3193f92
ChaitanyaM2004 <chaitanyajly0904@gmail.com>
2025-06-09 01:15:12 +0530

    updated header (auth app tag was not good looking)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Header = () => {
   return (
     <div className="bg-slate-200">
-        <div className="flex justify-between items-cente max-w-6xl mx-auto p-3">
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
             <Link to="/">
             <h1 className='font-bold'>Auth App</h1>
             </Link>
