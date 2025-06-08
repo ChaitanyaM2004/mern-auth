6c5495c29a8845cecf2140fd26edee753222a447
ChaitanyaM2004 <chaitanyajly0904@gmail.com>
2025-06-09 01:12:06 +0530

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from './pages/Home'
 import SignIn from './pages/Signin'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export const App = () => {
   return (
    <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <div className="bg-slate-200">
+        <div className="flex justify-between items-cente max-w-6xl mx-auto p-3">
+            <Link to="/">
+            <h1 className='font-bold'>Auth App</h1>
+            </Link>
+            <ul className='flex gap-4'>
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/about"><li>About</li></Link>
+                <Link to="/SignIn"><li>SignIn</li></Link>
+            </ul>
+
+        </div>
+        </div>
+  )
+}
+
+export default Header
