4a7e71ca12b41fdece76820dc63c65f3a4c7a961
ChaitanyaM2004 <chaitanyajly0904@gmail.com>
2025-06-10 01:44:48 +0530

    Add and use redux toolkit in our app for loading and error and storing user data

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,18 +1,19 @@
 import { React , useState ,useEffect} from 'react'
 import { Link , useNavigate } from 'react-router-dom'
+import { signInStart,signInFailure,signInSuccess } from '../redux/user/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
 export const SignIn = () => {
   const [formData , setFormData] = useState({})
-  const [error , setError] = useState(false)
-  const [loading,setLoading] = useState(false);
+  const { loading , error } = useSelector((state)=>state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e)=>{
      setFormData({...formData,[e.target.id]:e.target.value})
   }
   const handleSubmit = async(e)=>{
     e.preventDefault();
     try{
-      setLoading(true);
-      setError(false)
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin',{
       method:'POST',
       headers:{
@@ -21,15 +22,14 @@ export const SignIn = () => {
       body:JSON.stringify(formData),
     });
     const data = await res.json();
-    setLoading(false);
     if(data.success===false){
-      setError(true);
+      dispatch(signInFailure(data))
       return;
     }
+    dispatch(signInSuccess(data))
       navigate('/')
     } catch(error){
-       setLoading(false);
-       setError(true)
+       dispatch(signInFailure(error))
     }
   }
 
@@ -47,7 +47,7 @@ export const SignIn = () => {
            <span className='text-blue-500'>Sign up</span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'>{error&&"Something went wrong!"}</p>
+      <p className='text-red-700 mt-5'>{error?error.message||"Something went wrong!" : " "}</p>
     </div>
   )
 }
